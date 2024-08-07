190733b0927b44cf4d2028285c9f5c17fd5ca882
Hasnain49 <hasnain72.qureshi@gmail.com>
2024-08-07 18:34:20 +0500

    first

---
+++ frontend/src/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react'
+import Navbar from '../components/Navbar/Navbar'
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Text>Text</Text>
+    </>
+  )
+}
+
+export default Home
